c8aded2886c59cb7e104ea75468f76e761f086fb
Franck Owen <fxfrancky@yahoo.com>
2021-01-18 00:25:05 +0100

    adding all files

---
+++ Dockerfile
@@ -0,0 +1,30 @@
+# BUILD PHASE
+# Section ton install all dependencies (builder)
+FROM node:alpine as builder
+
+#Specify the working DIRECTORY
+WORKDIR '/usr/app'
+#Install some dependencies
+COPY package.json .
+#Install npm
+RUN npm install
+#Copy the remaining files
+COPY . .
+#Run npm with production parameter
+RUN npm run build
+
+
+
+
+
+
+
+
+
+
+#RUN PHASE
+#Section for the Nginx server
+FROM nginx
+# copy from "builder"
+#Get files from container wokdir/build to ngnx working dir /usr/share/nginx/html
+COPY --from=builder /usr/app/build /usr/share/nginx/html
--- src/App.js
+++ src/App.js
@@ -5,9 +5,9 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={logo} className="App-logo" alt="logo"/>
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          React is finally there  !!! WE WILL CHANGE.
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+         We want to Learn React
         </a>
       </header>
     </div>
